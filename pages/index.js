bf44c0687fea0ac78ce7db7e406f92742e71be1f
trixtu <trixtu12@gmail.com>
2024-04-24 20:34:01 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import SliderBlog from "@/src/components/ui/SliderBlog";
 import { GET_PAGE } from "@/src/queries/pages/get-page";
 import { sanitize } from "@/src/utils/miscellaneous";
 import { handleRedirectsAndReturnData } from "@/src/utils/slug";
+import { Tooltip } from "@chakra-ui/react";
+
 
 
 export default function Home({data}) {
@@ -22,6 +24,13 @@ export default function Home({data}) {
 			{ data?.page?.content ? <div className="mt-4" dangerouslySetInnerHTML={{__html: sanitize( data?.page?.content ?? {} )}}/> : null }
 
 			<SliderBlog blogs={data?.blogs?.edges}/>
+			<div class="fixed right-0 bottom-0 p-4 z-6">
+				<Tooltip hasArrow placement='auto-end' label='Ai nevoie de ajutor?' bg='#fff' color='black'>
+					<a href="https://wa.me/40757580129?text=" target="_blank">
+						<img src="/images/WhatsApp.png" width={"60"} alt=""/>
+					</a>
+				</Tooltip>
+			</div>
 		</Layout>
 	);
 }
